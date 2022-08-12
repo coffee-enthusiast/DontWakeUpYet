aced7a9032e713886e9564fe5918041a95363736
coffee-enthusiast <89907945+coffee-enthusiast@users.noreply.github.com>
2022-08-12 15:02:44 +0300

    Update Teleport.cs

--- Teleport.cs
+++ Teleport.cs
@@ -7,25 +7,12 @@ public class Teleport : MonoBehaviour
 {
 
     public Transform teleportPosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Teleport Pos: " + teleportPosition.position);
-        Debug.Log("My Pos: " + transform.position);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+   
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
 
-            Debug.Log("Teleport!" +other.gameObject.name + " pos: " + other.gameObject.transform.position);
             CharacterController c = other.gameObject.GetComponent<CharacterController>();
             StarterAssets.FirstPersonController f = other.gameObject.GetComponent<StarterAssets.FirstPersonController>();
             c.enabled = false;
@@ -33,7 +20,6 @@ public class Teleport : MonoBehaviour
             other.gameObject.transform.position = teleportPosition.position + new Vector3(1,0,0);
             c.enabled = true;
             f.enabled = true;
-            Debug.Log("Teleport after! Player pos: " + other.gameObject.transform.position);
         }
     }
 }
